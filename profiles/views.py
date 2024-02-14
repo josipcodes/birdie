51c5e3be0e0e12f97acbb6d4a93153b0b4afc988
Josip Potnar <j.potnar@gmail.com>
2024-02-14 08:39:10 +0000

    Add put function to profiles views

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Profile
 from .serializers import ProfileSerializer
 
@@ -27,7 +28,29 @@ class SpecificProfile(APIView):
         except Profile.DoesNotExist:
             raise Http404
 
+    # following two methods are copied from drf_api lessons
     def get(self, request, pk):
+        """
+        Obtains profile based on primary key
+        """
         profile = self.get_object(pk)
         serializer = ProfileSerializer(profile)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        """
+        Obtains profile based on primary key
+        Updates profile or serves 400
+        """
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(
+            profile, 
+            data=request.data
+            )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, 
+            status=status.HTTP_400_BAD_REQUEST
+            )
