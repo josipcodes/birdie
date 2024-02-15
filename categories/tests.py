e03fdb370cee5d82036414cc2aee5457abc0dc25
Josip Potnar <j.potnar@gmail.com>
2024-02-15 16:47:42 +0000

    Write tests for posts and categories

--- categories/tests.py
+++ categories/tests.py
@@ -8,4 +8,4 @@ class CategoryListTests(APITestCase):
     def test_can_list_categories(self):
         response = self.client.get('/categories/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print(response.data)
+
