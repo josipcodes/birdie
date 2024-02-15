43508acb81ca4c9b295c7712cf37c5d125c7b733
Josip Potnar <j.potnar@gmail.com>
2024-02-15 16:47:21 +0000

    Update category model to remove blank=True

--- categories/models.py
+++ categories/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=150)
-    description = models.TextField(blank=True)
+    description = models.TextField()
 
     def __str__(self):
         return self.name
