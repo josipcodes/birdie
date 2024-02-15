d4262bdd7919b1666442d6547de4b0e6f67eab0d
Josip Potnar <j.potnar@gmail.com>
2024-02-15 14:28:23 +0000

    Create Post CRUD

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 # importing views, renaming them to source
-from .views import PostList
+from .views import PostList, PostDetail
 
 urlpatterns = [
     path('posts/', PostList.as_view()),
+    path('posts/<int:pk>/', PostDetail.as_view()),
 ]
