d0e51abadc2bf67a0ff11cdf6e641ece9e5e9d01
Josip Potnar <j.potnar@gmail.com>
2024-02-15 15:25:01 +0000

    Update category model, create category test

--- categories/models.py
+++ categories/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=150)
-    description = models.TextField()
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
