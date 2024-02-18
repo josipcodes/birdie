186151bbfa6acb4e5d9028832ed890b489c21763
Josip Potnar <j.potnar@gmail.com>
2024-02-18 10:56:58 +0000

    Add placeholder navbar

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import styles from "../styles/NavBar.module.css"
+
+const NavBar = () => {
+  return (
+    // copied and modified
+    // source: https://react-bootstrap-v4.netlify.app/components/navbar/
+    <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
+        <i class="fa-brands fa-earlybirds"></i>
+      </Navbar.Brand>
+      <h2 className={`${styles.Brand} mr-auto`}>Birdie</h2>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">
+              Another action
+            </NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets">Log In</Nav.Link>
+          <Nav.Link eventKey={2} href="#memes">
+            Register
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default NavBar;
