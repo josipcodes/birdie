55cfed30a8265a5d665191a41a106cedfad8e1c3
Josip Potnar <j.potnar@gmail.com>
2024-02-18 13:14:06 +0000

    Add navbar css,, replace App css with module

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,41 +1,35 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import styles from "../styles/NavBar.module.css"
+import styles from "../styles/NavBar.module.css";
+
+const IconsLoggedIn = (
+  <>
+    <Nav.Link href="#features">Feed</Nav.Link>
+    <Nav.Link href="#pricing">Saved Posts</Nav.Link>
+  </>
+);
+
+const IconsLoggedOut = (
+  <>
+    <Nav.Link href="#deets">Log In</Nav.Link>
+    <Nav.Link href="#memes">Register</Nav.Link>
+  </>
+);
 
 const NavBar = () => {
   return (
     // copied and modified
     // source: https://react-bootstrap-v4.netlify.app/components/navbar/
-    <Navbar collapseOnSelect expand="sm" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="sm" className={styles.NavBar}>
       <Navbar.Brand href="#home" className="d-flex flex-row">
         <i className="fa-brands fa-earlybirds fa-lg mt-4 mr-2"></i>
         <h2 className={`${styles.Brand} mr-auto`}>Birdie</h2>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
-          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">
-              Another action
-            </NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-              Separated link
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <Nav.Link href="#deets">Log In</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            Register
-          </Nav.Link>
-        </Nav>
+        <Nav className="mr-auto">Logged in icons</Nav>
+        <Nav>Logged Out Icons</Nav>
       </Navbar.Collapse>
     </Navbar>
   );
