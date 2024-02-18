3bda409adb1762f37ef5ccb9c58d4a6eebc9a283
Josip Potnar <j.potnar@gmail.com>
2024-02-18 14:14:18 +0000

    Add placeholder for sign in form, hook route in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/Container';
+import { Route } from "react-router-dom";
+import LogInForm from './pages/auth/LogInForm';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container>
+        <h3>Register</h3>
+        <Route exact path='/login' render={() => <LogInForm />} />
+        <h3>Feed</h3>
+        <h3>Saved</h3>
+      </Container>
     </div>
   );
 }
