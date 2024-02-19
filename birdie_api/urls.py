bd71b243239585ea70ab56fc0e96feca4b08830a
Josip Potnar <j.potnar@gmail.com>
2024-02-19 15:54:45 +0000

    Add logout url in backend, add fix in views

--- birdie_api/urls.py
+++ birdie_api/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from .views import logout_route
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/api-auth/', include('rest_framework.urls')),
+    path('api/dj-rest-auth/logout/', logout_route),
     path('api/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/registration/', include('dj_rest_auth.registration.urls')), 
     path('api/', include('profiles.urls')),
