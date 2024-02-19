0b55069efe4d00d28ca036b758133382568b8478
Josip Potnar <j.potnar@gmail.com>
2024-02-19 14:10:24 +0000

    Add NavBar feature toggling depending on current user

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -2,22 +2,27 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import styles from "../styles/NavBar.module.css";
+import { SetCurrentUserContext } from "../App";
 
-const IconsLoggedIn = (
-  <>
-    <Nav.Link href="#features">Feed</Nav.Link>
-    <Nav.Link href="#pricing">Saved Posts</Nav.Link>
-  </>
-);
 
-const IconsLoggedOut = (
-  <>
-    <Nav.Link href="#deets">Log In</Nav.Link>
-    <Nav.Link href="#memes">Register</Nav.Link>
-  </>
-);
 
 const NavBar = () => {
+  const currentUser = SetCurrentUserContext
+
+  const IconsLoggedIn = (
+    <>
+      <Nav.Link href="#features">Feed</Nav.Link>
+      <Nav.Link href="#pricing">Saved Posts</Nav.Link>
+    </>
+  );
+  
+  const IconsLoggedOut = (
+    <>
+      <Nav.Link href="#deets">Log In</Nav.Link>
+      <Nav.Link href="#memes">Register</Nav.Link>
+    </>
+  );
+
   return (
     // copied and modified
     // source: https://react-bootstrap-v4.netlify.app/components/navbar/
@@ -28,7 +33,9 @@ const NavBar = () => {
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" className="navbar-dark" id={styles.Toggle} />
       <Navbar.Collapse id="responsive-navbar-nav">
+      { currentUser && IconsLoggedIn}
         <Nav className="mr-auto">Logged in icons</Nav>
+        { !currentUser && IconsLoggedOut}
         <Nav>Logged Out Icons</Nav>
       </Navbar.Collapse>
     </Navbar>
